a288dd8bc73145214d42b250a8b23652f92193f1
lasha tatulashvili <lashatatu@gmail.com>
2021-01-05 00:08:23 +0400

    start

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+const About = () => {
+    const a = 'hello world!'
+    return (
+        <h1> Hello about page -message {a} </h1>
+    )
+}
+
+// const About = () => {
+//     const a = 'hello world!'
+//     return React.createElement('h1',null,'I am generating this with createElement')
+// }
+
+
+export default About
