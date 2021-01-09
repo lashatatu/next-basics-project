54fd5afb85cbed9a1e439fff5233da5ea0f981e6
lasha tatulashvili <lashatatu@gmail.com>
2021-01-10 02:44:03 +0400

    Some basic styles

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,23 @@
 import React from 'react'
-const About = () => {
-    const a = 'hello world!'
-    return (
-        <h1> Hello about page -message {a} </h1>
-    )
-}
+// const About = () => {
+//     const a = 'hello world!'
+//     return (
+//         <h1> Hello about page -message {a} </h1>
+//     )
+// }
 
 // const About = () => {
 //     const a = 'hello world!'
 //     return React.createElement('h1',null,'I am generating this with createElement')
 // }
 
+class About extends React.Component {
+
+    render() {
+        return (
+            <h1>hello i am class componenta</h1>
+        );
+    }
+}
 
 export default About
